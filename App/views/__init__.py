0cee5f788828018e2a6cc01c0e64e1062b34a47a
Jiles Ramjattan <jilesramjattan16@gmail.com>
2024-04-17 14:15:54 +0000

    link home to web app

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -4,7 +4,8 @@ from .user import user_views
 from .index import index_views
 from .auth import auth_views
 from .home import home_views
+from .play import play_views
 
 
-views = [user_views, index_views, auth_views,home_views] 
+views = [user_views, index_views, auth_views,home_views,play_views] 
 # blueprints must be added to this list
--- App/views/play.py
+++ App/views/play.py
@@ -1,4 +1,22 @@
+from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
+from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 
-@auth_views.route('/play')
+from.index import index_views
+
+from App.controllers import (
+    create_user,
+    get_all_users,
+    get_all_users_json,
+    jwt_required
+)
+
+play_views = Blueprint('play_views', __name__, template_folder='../templates')
+@play_views.route('/play',methods=["GET"])
+@jwt_required()
 def play_page():
     return render_template("play.html")
+
+
+def get_daily_cipher():
+    
+    return cipher

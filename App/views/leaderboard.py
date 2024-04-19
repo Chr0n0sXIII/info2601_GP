d7fa1521ed68135f6b84201c7a91784578b6d8d6
uwishivastuff L <ramoutarshiva18@gmail.com>
2024-04-19 19:58:15 +0000

    updated

---
+++ App/views/leaderboard.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for,g
+from flask_jwt_extended import jwt_required, current_user as jwt_current_user
+
+from.index import index_views
+
+from App.controllers import (
+    create_user,
+    get_all_users,
+    get_all_users_json,
+    get_all_scores,
+    get_all_scores_json,
+    get_score,
+    jwt_required
+)
+
+leaderboard_views = Blueprint('leaderboard_views', __name__, template_folder='../templates')
+@leaderboard_views.route('/leaderboard',methods=["GET"])
+@jwt_required()
+def leaderboard_page():
+    users = get_all_users()
+    scores = get_all_scores()
+    userscores = []
+    leaderboardinfo = {}
+    lowestscore = 99
+    userscore = 99
+    for user in users:
+        print("userscore: ", get_score(user.id))
+        userscore = get_score(user.id)
+        if userscore == None:
+            userscore = 99
+        leaderboardinfo = {
+            "username":user.username,
+            "userscore":userscore
+        }
+        if userscore < lowestscore:
+            lowestscore = userscore
+        userscores.append(leaderboardinfo)
+        leaderboardinfo = {}
+    userscores.sort(key=lambda x: x['userscore'])
+
+    return render_template("leaderboard.html", allusers=users, allscores=scores, boardinfo=userscores)
+
+
+
+
